8bfe843891ff50a8905adb2a19b6bd58159f24eb
Alejo Arias <aarias@dotdash.com>
2019-09-19 12:42:46 +0200

    Add accent character to lower left most key

--- quantum/audio/song_list.h
+++ quantum/audio/song_list.h
@@ -161,7 +161,35 @@
 #define OLD_SPICE
 #define VICTORY_FANFARE_SHORT
 #define ALL_STAR
-#define RICK_ROLL
+#define RICK_ROLL Q__NOTE(_F4),   \
+Q__NOTE(_G4),   \
+Q__NOTE(_BF4),  \
+Q__NOTE(_G4), \
+HD_NOTE(_D5), \
+HD_NOTE(_D5),  \
+W__NOTE(_C5),  \
+S__NOTE(_REST),   \
+Q__NOTE(_F4),  \
+Q__NOTE(_G4), \
+Q__NOTE(_BF4), \
+Q__NOTE(_G4),  \
+HD_NOTE(_C5),  \
+HD_NOTE(_C5), \
+W__NOTE(_BF4),  \
+S__NOTE(_REST),   \
+Q__NOTE(_F4),  \
+Q__NOTE(_G4),  \
+Q__NOTE(_BF4), \
+Q__NOTE(_G4),  \
+W__NOTE(_BF4),  \
+H__NOTE(_C5),  \
+H__NOTE(_A4), \
+H__NOTE(_A4),  \
+H__NOTE(_G4), \
+H__NOTE(_F4),  \
+H__NOTE(_F4), \
+W__NOTE(_C5),  \
+W__NOTE(_BF4),
 #define FF_PRELUDE
 #define TO_BOLDLY_GO
 #define KATAWARE_DOKI
